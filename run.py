139ad4da2ca48bc6bb67a0a58d6ff7656f0f378b
Alex Terinte <terintealexandrin18@gmail.com>
2024-01-10 21:12:46 +0000

    add the bootsrap templete

---
+++ run.py
@@ -0,0 +1,22 @@
+import os
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/")
+def about():
+    return render_template("about.html")
+
+
+if __name__ == "__main__":
+    app.run(
+            host=os.environ.get("IP", "0.0.0.0"),
+            port=int(os.environ.get("PORT", "5000")),
+            debug=True)
