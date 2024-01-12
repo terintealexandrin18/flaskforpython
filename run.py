580677937f0886ba5155ca62a22f204553831a13
Alex Terinte <terintealexandrin18@gmail.com>
2024-01-12 20:45:08 +0000

    create file member

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import os
 import json
-
 from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
@@ -19,6 +19,17 @@ def about():
     return render_template("about.html", page_title="About", company=data)
 
 
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html", member=member)
+
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html", page_title="Contact")
@@ -28,8 +39,9 @@ def contact():
 def careers():
     return render_template("careers.html", page_title="Careers")
 
+
 if __name__ == "__main__":
     app.run(
-            host=os.environ.get("IP", "0.0.0.0"),
-            port=int(os.environ.get("PORT", "5000")),
-            debug=True)
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
